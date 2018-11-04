d4fe69fb95f2add83cda3aecc2a77e8a6c56313e
Joknoi <panupong.pj15@mail.kmutt.ac.th>
2018-11-04 18:17:07 +0700

    [ADD] Login

---
+++ ComfortPay/src/java/comfortpay/jpa/model/Account.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comfortpay.jpa.model;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Joknoi
+ */
+@Entity
+@Table(name = "ACCOUNT")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Account.findAll", query = "SELECT a FROM Account a")
+    , @NamedQuery(name = "Account.findByUsername", query = "SELECT a FROM Account a WHERE a.username = :username")
+    , @NamedQuery(name = "Account.findByPassword", query = "SELECT a FROM Account a WHERE a.password = :password")
+    , @NamedQuery(name = "Account.findByScore", query = "SELECT a FROM Account a WHERE a.score = :score")
+    , @NamedQuery(name = "Account.findByDegree", query = "SELECT a FROM Account a WHERE a.degree = :degree")})
+public class Account implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "USERNAME")
+    private String username;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "PASSWORD")
+    private String password;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "SCORE")
+    private int score;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "DEGREE")
+    private String degree;
+
+    public Account() {
+    }
+
+    public Account(String username) {
+        this.username = username;
+    }
+
+    public Account(String username, String password, int score, String degree) {
+        this.username = username;
+        this.password = password;
+        this.score = score;
+        this.degree = degree;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (username != null ? username.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Account)) {
+            return false;
+        }
+        Account other = (Account) object;
+        if ((this.username == null && other.username != null) || (this.username != null && !this.username.equals(other.username))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "comfortpay.jpa.model.Account[ username=" + username + " ]";
+    }
+    
+}
---
+++ ComfortPay/src/java/comfortpay/servlet/LoginServlet.java
@@ -0,0 +1,168 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comfortpay.servlet;
+
+import comfortpay.jpa.model.Account;
+import comfortpay.jpa.model.controller.AccountJpaController;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.annotation.Resource;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.transaction.UserTransaction;
+
+/**
+ *
+ * @author Joknoi
+ */
+public class LoginServlet extends HttpServlet {
+
+    @PersistenceUnit(unitName = "ComfortPayPU")
+    EntityManagerFactory emf;
+    @Resource
+    UserTransaction utx;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(true);
+        String path = request.getParameter("path");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if (session.getAttribute("account") == null) {
+            if (username != null || password != null) {
+                AccountJpaController accountCtrl = new AccountJpaController(utx, emf);
+                Account account = accountCtrl.findAccount(username);
+                if (account != null) {
+                    if (account.getPassword().equals(password)) {
+                        session.setAttribute("account", account);
+                        if (path == null || path == "") {
+                            getServletContext().getRequestDispatcher("/Home.jsp").forward(request, response);
+                            response.sendRedirect(path);
+                        } else {
+                            response.sendRedirect(path);
+                        }
+                    } else {
+                        request.setAttribute("path", path);
+                        getServletContext().getRequestDispatcher("/Login.jsp").forward(request, response);
+                    }
+                } else {
+                    request.setAttribute("path", path);
+                    getServletContext().getRequestDispatcher("/Login.jsp").forward(request, response);
+                }
+            } else {
+                request.setAttribute("path", path);
+                getServletContext().getRequestDispatcher("/Login.jsp").forward(request, response);
+            }
+        } else {
+            if (path == null || path == "") {
+                getServletContext().getRequestDispatcher("/Home.jsp").forward(request, response);
+            } else {
+                response.sendRedirect(path);
+            }
+        }
+
+//        if (path != null || path != "") {
+//            if (session.getAttribute("account") == null) {
+//                if (username != null || password != null) {
+//                    AccountJpaController accountCtrl = new AccountJpaController(utx, emf);
+//                    Account account = accountCtrl.findAccount(username);
+//                    if (account != null) {
+//                        if (account.getPassword().equals(password)) {
+//                            session.setAttribute("account", account);
+//                            response.sendRedirect("Home.jsp");
+//                        } else {
+//                            request.setAttribute("path", path);
+//                            getServletContext().getRequestDispatcher("/Login.jsp").forward(request, response);
+//                        }
+//                    } else {
+//                        request.setAttribute("path", path);
+//                        getServletContext().getRequestDispatcher("/Login.jsp").forward(request, response);
+//                    }
+//                } else {
+//                    request.setAttribute("path", path);
+//                    getServletContext().getRequestDispatcher("/Login.jsp").forward(request, response);
+//                }
+//            } else {
+//                response.sendRedirect(path);
+//            }
+//        } else {
+//            if (session.getAttribute("account") == null) {
+//                if (username != null || password != null) {
+//                    AccountJpaController accountCtrl = new AccountJpaController(utx, emf);
+//                    Account account = accountCtrl.findAccount(username);
+//                    if (account != null) {
+//                        if (account.getPassword().equals(password)) {
+//                            session.setAttribute("account", account);
+//                            getServletContext().getRequestDispatcher("/Home.jsp").forward(request, response);
+//                        } else {
+//                            getServletContext().getRequestDispatcher("/Login.jsp").forward(request, response);
+//                        }
+//                    } else {
+//                        getServletContext().getRequestDispatcher("/Login.jsp").forward(request, response);
+//                    }
+//                } else {
+//                    getServletContext().getRequestDispatcher("/Login.jsp").forward(request, response);
+//                }
+//            } else {
+//                getServletContext().getRequestDispatcher("/Home.jsp").forward(request, response);
+//            }
+//        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
